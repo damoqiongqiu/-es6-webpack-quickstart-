1ead21e1ec6681bfb3e16ed814b6932ec60920bf
damoqiongqiu <damoqiongqiu@126.com>
2019-05-11 21:55:35 +0800

    update-mock-server-and-package.json-config

--- mock-server.js
+++ mock-server.js
@@ -1,23 +1,25 @@
-let express = require('express');
-let Mock = require('mockjs');
+let express = require("express");
+let Mock = require("mockjs");
 let app = express();
 
-app.all('*', function (req, res, next) {
+app.all("*", function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By", ' 3.2.1')
+    res.header("X-Powered-By", " 3.2.1");
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
 
-app.use('/api/user', function (req, res) {
-    res.json(Mock.mock({
-        "name": "大漠穷秋",
-        "age": 18
-    }));
-})
+app.use("/api/user", function(req, res) {
+    res.json(
+        Mock.mock({
+            name: "大漠穷秋",
+            age: 18
+        })
+    );
+});
 
-app.listen('3000', () => {
-    console.log('mock server is listening at 3000');
+app.listen("3000", () => {
+    console.log("mock server is listening at 3000");
 });
